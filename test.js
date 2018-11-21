2892798add8d8f7fa2a045b2ab2e59e6b58196c4
Rafal Pijet <stronglopez@wp.pl>
2018-11-21 21:50:56 +0100

    Add task 8.2

--- test.js
+++ test.js
@@ -1,17 +1,58 @@
-var output = document.getElementById('greeter-output');
-
-output.innerHTML = 'Click the button! I want to say hello!' + '<br><br>' + output.innerHTML;
-
-var button = document.getElementById('greeter-button');
-
-var name;
-
-button.addEventListener('click', function(){
-    name = window.prompt('What is your name?');
-    if ((name == "null") || (name.length == 0)) {
-        output.innerHTML = "Error!!! You must enter name";
+var outputC = document.getElementById("greeter-outputC");
+var outputF = document.getElementById("greeter-outputF");
+var buttonC = document.getElementById("greeter-buttonC");
+var buttonF = document.getElementById("greeter-buttonF");
+var info = document.getElementById("info-field")
+var answer1 = "The water is frozen. Put on a warm jacket, hat and scarf. You can go skiing";
+var answer2 = "The water is liquid. You do not have to be warmly dressed. You can ride a bike";
+var answer3 = "The water is in the form of water vapor. Watch out because you're getting burned. You can make yourself a cup of tea";
+outputC.innerHTML = "Push the button to convert temperature from degrees Celsius to Fahrenheit";
+outputF.innerHTML = "Push the button to convert temperature from degrees Fahrenheit to Celsius";
+buttonC.addEventListener("click", function() {
+    var number = window.prompt("Enter the temperature in degrees Celsius");
+    var result;
+    number = parseInt(number);
+    if (isNaN(number)) {
+        outputC.innerHTML = "Error!!! You must enter number";
+        info.innerHTML = "Wait for information";
     }
     else {
-        output.innerHTML = 'Hello '+name+'!' + '<br><br>' + output.innerHTML;
+        result = number * 1.8 + 32;
+        outputC.innerHTML = "<br>" + "Temperature " + number + " Celsius degrees = " + Math.round(result) + " Fahrenheit degrees";
+        if (number <= 0) {
+            info.innerHTML = answer1;
+        }
+        else if ((number >= 0) && (number < 100)) {
+            info.innerHTML = answer2;
+        }
+        else {
+            info.innerHTML = answer3;
+        }
     }
 });
+buttonF.addEventListener("click", function () {
+    var number = window.prompt("Enter the temperature in degrees Fahrenheit");
+    var result;
+    number = parseInt(number);
+    if (isNaN(number)) {
+        outputF.innerHTML = "Error!!! You must enter number";
+        info.innerHTML = "Wait for information";
+    }
+    else {
+        result = (number - 32) / 2 * 1.1 ;
+        outputF.innerHTML = "<br>" + "Temperature " + number + " Fahrenheit degrees = " + Math.round(result) + " Celsius degrees";
+        if (result <= 0) {
+            info.innerHTML = answer1;
+        }
+        else if ((result >= 0) && (result < 100)) {
+            info.innerHTML = answer2;
+        }
+        else {
+            info.innerHTML = answer3;
+        }
+    }
+});
+
+
+
+
